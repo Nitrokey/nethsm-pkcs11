b3e7edfbef071bc4fb300aa746b399eee974b50b
Sven Anderson <sven@anderson.de>
2021-02-11 16:47:04 +0100

    WIP

--- tools/update_openapi.sh
+++ tools/update_openapi.sh
@@ -4,10 +4,9 @@ NETHSM_DIR=../nethsm
 API_DIR=api
 
 node $NETHSM_DIR/tools/api/convert_validate.js < $NETHSM_DIR/docs/nethsm-api.raml
+
 docker run --rm -ti -v "${PWD}:/local" \
   -e GO_POST_PROCESS_FILE="/usr/local/bin/gofmt -w" \
   openapitools/openapi-generator-cli generate -g go \
   -i /local/gen_nethsm_api_oas20.json -o /local/api \
-  -c /local/generator_conf.yaml
-
-rm api/go.*
+  -c /local/tools/generator_conf.yaml
