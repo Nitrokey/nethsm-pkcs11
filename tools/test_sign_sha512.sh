25ec2511ff9ec00070f1a01b216389db5de533ae
Robin Krahl <robin@nitrokey.com>
2021-05-11 13:26:32 +0200

    Update tools/test_sign_sha*.sh to use pre-hashing
    The NetHSM now requires pre-hashed messages.  This patch updates the
    tool/test_sign_sha*.sh scripts to calculate the message digest and set
    the appropriate hash algorithm.
    
    Note that the test_sign_sha1.sh script previously also used SHA-512.
    With this patch, we change it to use SHA-1 instead.

--- tools/test_sign_sha512.sh
+++ tools/test_sign_sha512.sh
@@ -9,8 +9,9 @@ rm _data.sig _public.pem
 curl -s -u operator:opPassphrase -X GET \
   https://nethsmdemo.nitrokey.com/api/v1/keys/$KEYID/public.pem -o _public.pem
 
-echo 'NetHSM rulez!' | pkcs11-tool --module p11nethsm.so -v -p opPassphrase \
-  --sign --mechanism SHA512-RSA-PKCS-PSS --output-file _data.sig --id $HEXID
+echo 'NetHSM rulez!' | openssl dgst -sha512 -binary | pkcs11-tool \
+  --module p11nethsm.so -v -p opPassphrase --sign --mechanism RSA-PKCS-PSS \
+  --hash-algorithm SHA512 --output-file _data.sig --id $HEXID
 
 echo 'NetHSM rulez!' | openssl dgst -keyform PEM -verify _public.pem -sha512 \
   -sigopt rsa_padding_mode:pss -sigopt rsa_pss_saltlen:-1 -signature _data.sig
