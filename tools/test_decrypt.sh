dc9632abf2aa3e37c4c70cc047efc4cd5811ed83
Nils Ponsard | nitrokey <nils@nitrokey.com>
2023-07-10 11:42:54 +0200

    feat: fix decrypt test scripts

--- tools/test_decrypt.sh
+++ tools/test_decrypt.sh
@@ -2,7 +2,7 @@
 
 KEYID=$1
 
-HEXID=$(echo -n ${KEYID}'\c' | xxd -ps)
+HEXID=$(echo -n ${KEYID} | xxd -ps)
 
 rm -rf _data.crypt _public.pem
 
@@ -12,6 +12,6 @@ curl -k -s -u operator:opPassphrase -X GET \
 echo 'NetHSM rulez!' | openssl rsautl -encrypt -inkey _public.pem -pubin \
   -out _data.crypt
 
-pkcs11-tool --module ./target/debug/libnethsm_pkcs11.so  -v -p opPassphrase --decrypt \
+pkcs11-tool --module ./target/debug/libnethsm_pkcs11.so  -v --decrypt \
   --mechanism RSA-PKCS --input-file _data.crypt --id $HEXID
 
--- tools/test_decrypt_sha512.sh
+++ tools/test_decrypt_sha512.sh
@@ -2,7 +2,7 @@
 
 KEYID=$1
 
-HEXID=$(echo -n ${KEYID}'\c' | xxd -ps)
+HEXID=$(echo -n ${KEYID} | xxd -ps)
 
 rm -rf _data.crypt _public.pem
 
@@ -13,6 +13,6 @@ echo 'NetHSM rulez!' | openssl pkeyutl -encrypt -pubin -inkey _public.pem \
   -pkeyopt rsa_padding_mode:oaep -pkeyopt rsa_oaep_md:sha512 \
   -pkeyopt rsa_mgf1_md:sha512 -out _data.crypt
 
-pkcs11-tool --module ./target/debug/libnethsm_pkcs11.so  -v -p opPassphrase --decrypt \
+pkcs11-tool --module ./target/debug/libnethsm_pkcs11.so  -v --decrypt \
   --mechanism RSA-PKCS-OAEP --input-file _data.crypt --id $HEXID \
   --hash-algorithm SHA512
