4dfe4dcdc84143775aa258a67af7fc87f0d7963d
Nils Ponsard | Nitrokey <nils@nitrokey.com>
2023-07-12 13:19:47 +0200

    fix(ci): use arch linux to get the latest opensc

--- tools/update_openapi.sh
+++ tools/update_openapi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm -u $UID -v "${PWD}/openapi:/out" -v "${PWD}/tools/generator_conf.yaml:/conf.yaml" openapitools/openapi-generator-cli generate -i=https://nethsmdemo.nitrokey.com/api_docs/nethsm-api.yaml -o out -g rust -c /conf.yaml
+docker run --rm -u $UID -v "${PWD}/openapi:/out" -v "${PWD}/tools/generator_conf.yaml:/conf.yaml" openapitools/openapi-generator-cli:latest generate -i=https://nethsmdemo.nitrokey.com/api_docs/nethsm-api.yaml -o out -g rust -c /conf.yaml
 cargo fmt
