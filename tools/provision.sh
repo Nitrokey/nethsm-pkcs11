c9ff50302a96dbbb471a386ae49237af0f372e6e
Nils Ponsard | Nitrokey <nils@nitrokey.com>
2023-07-12 12:37:36 +0200

    fix(ci): admin password

--- tools/provision.sh
+++ tools/provision.sh
@@ -2,4 +2,4 @@
 
 curl -k -i  \
   https://localhost:8443/api/v1/provision  -X POST \
-  -H "content-type: application/json" -d '{"unlockPassphrase": "UnlockPassphrase",  "adminPassphrase": "AdminPassphrase", "systemTime": "2018-10-30T11:20:50Z"  }'
+  -H "content-type: application/json" -d '{"unlockPassphrase": "UnlockPassphrase",  "adminPassphrase": "Administrator", "systemTime": "2018-10-30T11:20:50Z"  }'
