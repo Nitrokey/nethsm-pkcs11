51af25196afee0bd376a33f385ade28d715e1260
Nils Ponsard | Nitrokey <nils@nitrokey.com>
2023-08-01 11:42:29 +0200

    feat: create patch file for automated update of the openapi crate

--- tools/update_openapi.sh
+++ tools/update_openapi.sh
@@ -2,3 +2,4 @@
 
 docker run --rm -u $UID -v "${PWD}/openapi:/out" -v "${PWD}/tools/generator_conf.yaml:/conf.yaml" openapitools/openapi-generator-cli:latest generate -i=https://nethsmdemo.nitrokey.com/api_docs/nethsm-api.yaml -o out -g rust -c /conf.yaml
 cargo fmt
+git apply tools/0001-patch-openapi.patch
