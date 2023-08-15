4833d0cf1b377b2135ee8c1e7f47629ed36d93b0
Nils Ponsard | Nitrokey <nils@nitrokey.com>
2023-08-15 15:51:56 +0200

    feat: better rsa key tests

--- tools/tests/decrypt_rsa_pkcs_sha512.sh
+++ tools/tests/decrypt_rsa_pkcs_sha512.sh
@@ -4,15 +4,19 @@ KEYID=rsakey
 
 HEXID=$(echo -n ${KEYID} | xxd -ps)
 
-rm -rf _data.crypt _public.pem
+rm -rf _data.crypt _public.pem _input _data.decrypt
 
 curl -k -s --fail-with-body -u operator:opPassphrase -X GET \
   https://localhost:8443/api/v1/keys/$KEYID/public.pem -o _public.pem
 
-echo 'NetHSM rulez!NetHSM rulez!' | openssl pkeyutl -encrypt -pubin -inkey _public.pem \
-  -pkeyopt rsa_padding_mode:oaep -pkeyopt rsa_oaep_md:sha256 \
-  -pkeyopt rsa_mgf1_md:sha256 -out _data.crypt
+echo 'NetHSM rulez!' > _input 
+
+openssl pkeyutl -encrypt -pubin -inkey _public.pem \
+  -pkeyopt rsa_padding_mode:oaep -pkeyopt rsa_oaep_md:sha512 \
+  -pkeyopt rsa_mgf1_md:sha512 -out _data.crypt -in _input
 
 pkcs11-tool --module ./target/debug/libnethsm_pkcs11.so  -v --decrypt \
   --mechanism RSA-PKCS-OAEP --input-file _data.crypt --id $HEXID \
-  --hash-algorithm SHA256
+  --hash-algorithm SHA512 --output-file _data.decrypt
+
+diff _input _data.decrypt
