3b7966049a7fd556b152c87027a51f210bee92bd
Nils Ponsard | Nitrokey <nils@nitrokey.com>
2023-08-15 12:07:45 +0200

    feat: EDDSA tests

--- tools/ci_integration_tests.sh
+++ tools/ci_integration_tests.sh
@@ -12,9 +12,11 @@ export P11NETHSM_CONFIG_FILE=./p11nethsm.conf
 ./tools/tests/decrypt_rsa.sh
 ./tools/tests/encrypt_aes.sh
 ./tools/tests/sign_ec_p256.sh
+./tools/tests/sign_ed.sh
 ./tools/tests/sign_rsa.sh
 ./tools/tests/sign_rsa_sha1.sh
 ./tools/tests/sign_rsa_sha256.sh
+./tools/tests/upload_ed_key_id.sh
 ./tools/tests/upload_ec_key.sh
 ./tools/tests/upload_ec_key_id.sh
 ./tools/tests/upload_aes_key.sh
@@ -24,6 +26,7 @@ export P11NETHSM_CONFIG_FILE=./p11nethsm.conf
 ./tools/tests/delete_key.sh
 ./tools/tests/generate_rsa_id.sh
 ./tools/tests/generate_ec_id.sh
+./tools/tests/generate_ed_id.sh
 ./tools/tests/generate_generic_id.sh
 
 
