4a6fe265f978fb0d7c0785b0a1a7b718864d0aa1
Nils Ponsard | Nitrokey <nils@nitrokey.com>
2023-08-28 18:07:45 +0200

    feat: certificate verification with fingerprint

--- pkcs11/src/config/initialization.rs
+++ pkcs11/src/config/initialization.rs
@@ -1,11 +1,12 @@
 use std::sync::{Arc, Mutex};
 
-use rustls::client::ServerCertVerifier;
-
 use super::{
     config_file::SlotConfig,
     device::{Device, Slot},
 };
+use log::trace;
+use rustls::client::ServerCertVerifier;
+use sha2::Digest;
 
 const DEFAULT_USER_AGENT: &str = "pkcs11-rs/0.1.0";
 
@@ -50,13 +51,37 @@ impl ServerCertVerifier for DangerIgnoreVerifier {
     }
 }
 
+struct FingerprintVerifier {
+    fingerprints: Vec<Vec<u8>>,
+}
+
+impl ServerCertVerifier for FingerprintVerifier {
+    fn verify_server_cert(
+        &self,
+        end_entity: &rustls::Certificate,
+        _intermediates: &[rustls::Certificate],
+        _server_name: &rustls::ServerName,
+        _scts: &mut dyn Iterator<Item = &[u8]>,
+        _ocsp_response: &[u8],
+        _now: std::time::SystemTime,
+    ) -> Result<rustls::client::ServerCertVerified, rustls::Error> {
+        let mut hasher = sha2::Sha256::new();
+        hasher.update(&end_entity.0);
+        let result = hasher.finalize();
+        for fingerprint in self.fingerprints.iter() {
+            if fingerprint == &result[..] {
+                trace!("Certificate fingerprint matches");
+                return Ok(rustls::client::ServerCertVerified::assertion());
+            }
+        }
+        Err(rustls::Error::General(
+            "Could not verify certificate fingerprint".to_string(),
+        ))
+    }
+}
+
 fn slot_from_config(slot: &SlotConfig) -> Result<Slot, InitializationError> {
     let mut instances = vec![];
-
-    let mut roots = rustls::RootCertStore::empty();
-    for cert in rustls_native_certs::load_native_certs().expect("could not load platform certs") {
-        roots.add(&rustls::Certificate(cert.0)).unwrap();
-    }
 
     let default_user = slot
         .operator
@@ -67,23 +92,32 @@ fn slot_from_config(slot: &SlotConfig) -> Result<Slot, InitializationError> {
     for instance in slot.instances.iter() {
         let tls_conf = rustls::ClientConfig::builder().with_safe_defaults();
 
+        println!("{:?}", instance.sha256_fingerprints);
+
         let tls_conf = if instance.danger_insecure_cert {
             tls_conf
                 .with_custom_certificate_verifier(Arc::new(DangerIgnoreVerifier {}))
                 .with_no_client_auth()
-        } else {
+        } else if !instance.sha256_fingerprints.is_empty() {
+            let mut fingerprints = vec![];
+
+            for fingerprint in instance.sha256_fingerprints.iter() {
+                fingerprints.push(hex::decode(fingerprint.replace(':', "")).unwrap());
+            }
+
             tls_conf
-                .with_root_certificates(roots.clone())
+                .with_custom_certificate_verifier(Arc::new(FingerprintVerifier { fingerprints }))
                 .with_no_client_auth()
+        } else {
+            let mut roots = rustls::RootCertStore::empty();
+            for cert in
+                rustls_native_certs::load_native_certs().expect("could not load platform certs")
+            {
+                roots.add(&rustls::Certificate(cert.0)).unwrap();
+            }
+
+            tls_conf.with_root_certificates(roots).with_no_client_auth()
         };
-        // TODO : re-implement cert verification
-        // else if let Some(cert_str) = instance.certificate.as_ref() {
-        //     let mut roots = rustls::RootCertStore::empty();
-        //     let cert = Certificate::(cert_str.trim().as_bytes())
-        //         .map_err(InitializationError::Reqwest)?;
-        //     roots.add(&rustls::Certificate(cert.0)).unwrap();
-        //     tls_conf.with_root_certificates(roots).with_no_client_auth()
-        // }
 
         let agent = ureq::AgentBuilder::new()
             .tls_config(Arc::new(tls_conf))
