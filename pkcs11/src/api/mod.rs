607fdd36d119aa8728fd9ee4b10af3b9db555698
Nils Ponsard | Nitrokey <nils@nitrokey.com>
2023-08-30 15:24:38 +0200

    fix: reading args on windows

--- pkcs11/src/api/mod.rs
+++ pkcs11/src/api/mod.rs
@@ -54,23 +54,20 @@ pub extern "C" fn C_Initialize(pInitArgs: CK_VOID_PTR) -> CK_RV {
             return cryptoki_sys::CKR_ARGUMENTS_BAD;
         }
 
-        trace!(
-            "C_Initialize() called with CKF_OS_LOCKING: {:?}",
-            args.flags
-        );
-        trace!(
-            "C_Initialize() called with CreateMutex: {:?}",
-            args.CreateMutex
-        );
+        let flags = args.flags;
+        let CreateMutex = args.CreateMutex;
+
+        trace!("C_Initialize() called with flags: {:?}", flags);
+        trace!("C_Initialize() called with CreateMutex: {:?}", CreateMutex);
 
         // currently we don't support custom locking
         // if the flag is not set and the mutex functions are not null, the program asks us to use only the mutex functions, we can't do that
-        if args.flags & cryptoki_sys::CKF_OS_LOCKING_OK == 0 && args.CreateMutex.is_some() {
+        if flags & cryptoki_sys::CKF_OS_LOCKING_OK == 0 && CreateMutex.is_some() {
             return cryptoki_sys::CKR_CANT_LOCK;
         }
 
         // currently we are using tokio that needs to create threads, so if the programs forbids us to create threads we return an error
-        if args.flags & cryptoki_sys::CKF_LIBRARY_CANT_CREATE_OS_THREADS != 0 {
+        if flags & cryptoki_sys::CKF_LIBRARY_CANT_CREATE_OS_THREADS != 0 {
             return cryptoki_sys::CKR_NEED_TO_CREATE_THREADS;
         }
     }
