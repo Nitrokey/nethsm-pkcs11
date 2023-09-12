56c1e006e939ccc908e23e51a74b90cc80008194
Nils Ponsard | Nitrokey <nils@nitrokey.com>
2023-09-12 11:13:30 +0200

    feat: add rsa pkcs tests with pre-hashing

---
+++ tools/tests/sign_rsa_pkcs_pss_sha384.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -x
+
+set -e 
+KEYID=rsakey
+
+HEXID=$(echo -n ${KEYID} | xxd -ps)
+
+rm -rf _data.sig _public.pem
+
+curl -s --fail-with-body -k -u operator:opPassphrase -v -X GET \
+  https://localhost:8443/api/v1/keys/$KEYID/public.pem -o _public.pem
+
+echo 'NetHSM rulez!' | pkcs11-tool --module ./target/debug/libnethsm_pkcs11.so  -v \
+  --sign --mechanism SHA384-RSA-PKCS-PSS --output-file _data.sig --id $HEXID 
+
+echo 'NetHSM rulez!' | openssl dgst -keyform PEM -verify _public.pem -sha384 \
+  -sigopt rsa_padding_mode:pss -sigopt rsa_pss_saltlen:-1 -signature _data.sig
---
+++ tools/tests/sign_rsa_pkcs_sha512.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -x
+
+set -e 
+KEYID=rsakey
+
+HEXID=$(echo -n ${KEYID} | xxd -ps)
+
+rm -rf _data.sig _public.pem
+
+curl -s --fail-with-body -k -u operator:opPassphrase -v -X GET \
+  https://localhost:8443/api/v1/keys/$KEYID/public.pem -o _public.pem
+
+echo 'NetHSM rulez!' | pkcs11-tool --module ./target/debug/libnethsm_pkcs11.so  -v \
+  --sign --mechanism SHA512-RSA-PKCS --output-file _data.sig --id $HEXID --signature-format openssl
+
+echo 'NetHSM rulez!' | openssl dgst -sha512 -binary | openssl pkeyutl -verify -inkey _public.pem -sigfile _data.sig -pubin
