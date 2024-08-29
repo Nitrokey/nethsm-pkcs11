1436d2e57a033fe04927fd166e7563468ee86605
Sosthène Guédon <sosthene@nitrokey.com>
2024-08-29 17:54:57 +0200

    Fix CI

--- tools/test_profiling.sh
+++ tools/test_profiling.sh
@@ -7,10 +7,10 @@ export LLVM_PROFILE_FILE="${PWD}/profile/%p-%m.profraw"
 rm -rf _test_objects
 
 # run normal tests
-RUSTFLAGS="-C instrument-coverage" cargo test --all-features --all-targets
+RUSTFLAGS="-C instrument-coverage" cargo test --all-targets
 
 # run tests that require to be run in a single thread
-RUSTFLAGS="-C instrument-coverage" cargo test --all-features --all-targets -- --test-threads=1  --ignored
+RUSTFLAGS="-C instrument-coverage" cargo test --all-targets -- --test-threads=1  --ignored
 
 
 files=$(RUSTFLAGS="-C instrument-coverage" cargo test --tests --no-run --message-format=json | jq -r "select(.profile.test == true) | .filenames[]" | grep -v dSYM - )
