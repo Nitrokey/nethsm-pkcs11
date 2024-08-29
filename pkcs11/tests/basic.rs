c077c6a8c310c0a587bbf869b19023217a739571
Sosthène Guédon <sosthene@nitrokey.com>
2024-08-29 15:38:24 +0200

    Factory reset the container for each test

--- pkcs11/tests/basic.rs
+++ pkcs11/tests/basic.rs
@@ -1,11 +1,60 @@
 #![cfg(feature = "pkcs11-full-tests")]
 
+use core::ptr;
+
 use config_file::{InstanceConfig, P11Config, SlotConfig, UserConfig};
+use pkcs11::types::{
+    CKA_MODULUS_BITS, CKA_PUBLIC_EXPONENT, CKA_SIGN, CKA_TOKEN, CKA_VERIFY, CKM_RSA_PKCS,
+    CK_ATTRIBUTE, CK_BBOOL, CK_FALSE, CK_MECHANISM, CK_TRUE, CK_ULONG,
+};
 
 mod tools;
 
+const RSA_PRIVATE_KEY_ATTRIBUTES: &[CK_ATTRIBUTE] = &[
+    CK_ATTRIBUTE {
+        attrType: CKA_SIGN,
+        pValue: &CK_TRUE as *const _ as *mut _,
+        ulValueLen: size_of::<CK_BBOOL>() as _,
+    },
+    CK_ATTRIBUTE {
+        attrType: CKA_TOKEN,
+        pValue: &CK_FALSE as *const _ as *mut _,
+        ulValueLen: size_of::<CK_BBOOL>() as _,
+    },
+];
+
+const RSA_PUBLIC_KEY_ATTRIBUTES: &[CK_ATTRIBUTE] = &[
+    CK_ATTRIBUTE {
+        attrType: CKA_VERIFY,
+        pValue: &CK_TRUE as *const _ as *mut _,
+        ulValueLen: size_of::<CK_BBOOL>() as _,
+    },
+    CK_ATTRIBUTE {
+        attrType: CKA_MODULUS_BITS,
+        pValue: &(2048 as CK_ULONG) as *const _ as *mut _,
+        ulValueLen: size_of::<CK_ULONG>() as _,
+    },
+    CK_ATTRIBUTE {
+        attrType: CKA_TOKEN,
+        pValue: &CK_FALSE as *const _ as *mut _,
+        ulValueLen: size_of::<CK_BBOOL>() as _,
+    },
+    CK_ATTRIBUTE {
+        attrType: CKA_PUBLIC_EXPONENT,
+        pValue: [0x01, 0x00, 0x01].as_ptr() as *mut _,
+        ulValueLen: 3 as _,
+    },
+];
+
+const RSA_MECHANISM: CK_MECHANISM = CK_MECHANISM {
+    mechanism: CKM_RSA_PKCS,
+    pParameter: ptr::null_mut(),
+    ulParameterLen: 0,
+};
+
 #[test]
 fn basic() {
+    env_logger::init();
     tools::run_tests(
         P11Config {
             slots: vec![SlotConfig {
@@ -14,10 +63,15 @@ fn basic() {
                     username: "operator".into(),
                     password: Some("opPassphrase".into()),
                 }),
-                administrator: None,
+                administrator: Some(UserConfig {
+                    username: "admin".into(),
+                    password: Some("Administrator".into()),
+                }),
                 description: Some("Test slot".into()),
                 instances: vec![InstanceConfig {
-                    url: "https://localhost:8443/api/v1".into(),
+                    url: option_env!("TEST_NETHSM_INSTANCE")
+                        .unwrap_or("https://localhost:8443/api/v1")
+                        .into(),
                     danger_insecure_cert: true,
                     sha256_fingerprints: Vec::new(),
                     max_idle_connections: None,
@@ -28,8 +82,21 @@ fn basic() {
             ..Default::default()
         },
         |ctx| {
-            let session = 0;
-            ctx.open_session(session, 0x04, None, None).unwrap();
+            let slot = 0;
+            let session = ctx.open_session(slot, 0x04, None, None).unwrap();
+            let (_public_key, private_key) = ctx
+                .generate_key_pair(
+                    session,
+                    &RSA_MECHANISM,
+                    RSA_PUBLIC_KEY_ATTRIBUTES,
+                    RSA_PRIVATE_KEY_ATTRIBUTES,
+                )
+                .unwrap();
+            let data = [0x42; 32];
+            ctx.sign_init(session, &RSA_MECHANISM, private_key).unwrap();
+
+            // Verifying signatures is not supported
+            let _signature = ctx.sign(session, &data).unwrap();
         },
     )
 }
--- pkcs11/tests/tools/mod.rs
+++ pkcs11/tests/tools/mod.rs
@@ -1,12 +1,189 @@
-use std::env::set_var;
-use std::io::BufWriter;
+use std::io::{BufWriter, Read};
+use std::process::{Child, Stdio};
+use std::sync::Arc;
+use std::thread::sleep;
+use std::time::Duration;
+use std::{env::set_var, process::Command};
 
 pub use config_file::P11Config;
 
+use nethsm_sdk_rs::{
+    apis::{
+        configuration::Configuration,
+        default_api::{provision_post, users_user_id_put},
+    },
+    models::{ProvisionRequestData, UserPostData, UserRole},
+};
 use pkcs11::Ctx;
+use rustls::{
+    client::danger::ServerCertVerifier,
+    crypto::{verify_tls12_signature, verify_tls13_signature, CryptoProvider},
+};
 use tempfile::NamedTempFile;
+use time::format_description;
+use ureq::AgentBuilder;
+
+pub const TEST_NETHSM_INSTANCE: &str = match option_env!("TEST_NETHSM_INSTANCE") {
+    Some(v) => v,
+    None => "https://localhost:8443/api/v1",
+};
+
+#[derive(Debug)]
+struct DangerIgnoreVerifier;
+
+impl ServerCertVerifier for DangerIgnoreVerifier {
+    fn verify_server_cert(
+        &self,
+        _end_entity: &rustls::pki_types::CertificateDer<'_>,
+        _intermediates: &[rustls::pki_types::CertificateDer<'_>],
+        _server_name: &rustls::pki_types::ServerName<'_>,
+        _ocsp_response: &[u8],
+        _now: rustls::pki_types::UnixTime,
+    ) -> Result<rustls::client::danger::ServerCertVerified, rustls::Error> {
+        Ok(rustls::client::danger::ServerCertVerified::assertion())
+    }
+
+    fn verify_tls12_signature(
+        &self,
+        message: &[u8],
+        cert: &rustls::pki_types::CertificateDer<'_>,
+        dss: &rustls::DigitallySignedStruct,
+    ) -> Result<rustls::client::danger::HandshakeSignatureValid, rustls::Error> {
+        let default_provider = CryptoProvider::get_default().unwrap();
+        verify_tls12_signature(
+            message,
+            cert,
+            dss,
+            &default_provider.signature_verification_algorithms,
+        )
+    }
+
+    fn verify_tls13_signature(
+        &self,
+        message: &[u8],
+        cert: &rustls::pki_types::CertificateDer<'_>,
+        dss: &rustls::DigitallySignedStruct,
+    ) -> Result<rustls::client::danger::HandshakeSignatureValid, rustls::Error> {
+        let default_provider = CryptoProvider::get_default().unwrap();
+        verify_tls13_signature(
+            message,
+            cert,
+            dss,
+            &default_provider.signature_verification_algorithms,
+        )
+    }
+
+    fn supported_verify_schemes(&self) -> Vec<rustls::SignatureScheme> {
+        let default_provider = CryptoProvider::get_default().unwrap();
+
+        default_provider
+            .signature_verification_algorithms
+            .supported_schemes()
+    }
+}
+
+fn tls_conf() -> rustls::ClientConfig {
+    rustls::crypto::ring::default_provider()
+        .install_default()
+        .ok();
+    rustls::ClientConfig::builder()
+        .dangerous()
+        .with_custom_certificate_verifier(Arc::new(DangerIgnoreVerifier))
+        .with_no_client_auth()
+}
+
+struct Dropper(Child);
+
+impl Drop for Dropper {
+    fn drop(&mut self) {
+        Command::new("kill")
+            .args([self.0.id().to_string()])
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
+        self.0.wait().unwrap();
+        let mut buf = String::new();
+        self.0
+            .stdout
+            .take()
+            .unwrap()
+            .read_to_string(&mut buf)
+            .unwrap();
+        buf.push('\n');
+        self.0
+            .stderr
+            .take()
+            .unwrap()
+            .read_to_string(&mut buf)
+            .unwrap();
+        println!("{buf}");
+    }
+}
 
 pub fn run_tests(config: P11Config, f: impl FnOnce(&mut Ctx)) {
+    let _dropper = Dropper(
+        Command::new("podman")
+            .args([
+                "run",
+                "--rm",
+                "-ti",
+                "-p8443:8443",
+                "-p8080:8080",
+                "docker.io/nitrokey/nethsm:testing",
+            ])
+            .stdin(Stdio::null())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .unwrap(),
+    );
+
+    let client = AgentBuilder::new().tls_config(Arc::new(tls_conf())).build();
+
+    let sdk_config = Configuration {
+        client,
+        base_path: TEST_NETHSM_INSTANCE.into(),
+        basic_auth: Some(("admin".into(), Some("Administrator".into()))),
+        ..Default::default()
+    };
+
+    sleep(Duration::from_secs(2));
+
+    // match system_factory_reset_post(&sdk_config) {
+    //     Ok(_) => {}
+    //     Err(nethsm_sdk_rs::apis::Error::ResponseError(ResponseContent {
+    //         entity: SystemFactoryResetPostError::Status412(),
+    //         ..
+    //     })) => {}
+    //     Err(nethsm_sdk_rs::apis::Error::Ureq(ureq::Error::Status(412, _))) => {}
+    //     Err(e) => panic!("{e}"),
+    // };
+    provision_post(
+        &sdk_config,
+        ProvisionRequestData {
+            unlock_passphrase: "1234567890".into(),
+            admin_passphrase: "Administrator".into(),
+            system_time: time::OffsetDateTime::now_utc()
+                .format(
+                    &format_description::parse("[year]-[month]-[day]T[hour]:[minute]:[second]Z")
+                        .unwrap(),
+                )
+                .unwrap(),
+        },
+    )
+    .unwrap();
+    users_user_id_put(
+        &sdk_config,
+        "operator",
+        UserPostData {
+            real_name: "Operator".into(),
+            role: UserRole::Operator,
+            passphrase: "opPassphrase".into(),
+        },
+    )
+    .unwrap();
+
     let mut tmpfile: NamedTempFile = NamedTempFile::new().unwrap();
 
     serde_yaml::to_writer(BufWriter::new(tmpfile.as_file_mut()), &config).unwrap();
@@ -14,4 +191,5 @@ pub fn run_tests(config: P11Config, f: impl FnOnce(&mut Ctx)) {
     set_var(config_file::ENV_VAR_CONFIG_FILE, path);
     let mut ctx = Ctx::new_and_initialize("../target/release/libnethsm_pkcs11.so").unwrap();
     f(&mut ctx);
+    ctx.close_all_sessions(0).unwrap();
 }
