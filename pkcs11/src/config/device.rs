366ae570e456410c6c21a428cb3348b0cb6cf5b0
Sosthène Guédon <sosthene@nitrokey.com>
2024-09-11 12:03:37 +0200

    Run regular retries for new failed instances

--- pkcs11/src/config/device.rs
+++ pkcs11/src/config/device.rs
@@ -1,13 +1,75 @@
 use std::{
-    sync::{atomic::AtomicUsize, Arc, Condvar, Mutex, RwLock},
-    time::Instant,
+    collections::BTreeMap,
+    sync::{
+        atomic::{AtomicUsize, Ordering::Relaxed},
+        mpsc::{self, RecvTimeoutError},
+        Arc, Condvar, LazyLock, Mutex, RwLock,
+    },
+    thread,
+    time::{Duration, Instant},
 };
 
-use nethsm_sdk_rs::apis::configuration::Configuration;
+use nethsm_sdk_rs::apis::{configuration::Configuration, default_api::health_ready_get};
 
-use crate::backend::db::Db;
+use crate::{backend::db::Db, data::THREADS_ALLOWED};
 
 use super::config_file::{RetryConfig, UserConfig};
+
+static RETRY_THREAD: LazyLock<mpsc::Sender<(Duration, InstanceData)>> = LazyLock::new(|| {
+    let (tx, rx) = mpsc::channel();
+    let (tx_instance, rx_instance) = mpsc::channel();
+    thread::spawn(background_thread(rx_instance));
+    thread::spawn(background_timer(rx, tx_instance));
+    tx
+});
+
+fn background_timer(
+    rx: mpsc::Receiver<(Duration, InstanceData)>,
+    tx_instance: mpsc::Sender<InstanceData>,
+) -> impl FnOnce() {
+    let mut jobs: BTreeMap<Instant, InstanceData> = BTreeMap::new();
+    move || loop {
+        let next_job = jobs.pop_first();
+        let Some((next_job_deadline, next_job_instance)) = next_job else {
+            // No jobs in the queue, we can just run the next
+            let Ok((new_job_duration, new_state)) = rx.recv() else {
+                return;
+            };
+
+            jobs.insert(Instant::now() + new_job_duration, new_state);
+            continue;
+        };
+
+        let now = Instant::now();
+
+        if now >= next_job_deadline {
+            tx_instance.send(next_job_instance).unwrap();
+            continue;
+        }
+        jobs.insert(next_job_deadline, next_job_instance);
+
+        let timeout = next_job_deadline.duration_since(now);
+        match rx.recv_timeout(timeout) {
+            Ok((run_in, new_instance)) => {
+                jobs.insert(now + run_in, new_instance);
+                continue;
+            }
+            Err(RecvTimeoutError::Timeout) => continue,
+            Err(RecvTimeoutError::Disconnected) => break,
+        }
+    }
+}
+
+fn background_thread(rx: mpsc::Receiver<InstanceData>) -> impl FnOnce() {
+    move || loop {
+        while let Ok(instance) = rx.recv() {
+            match health_ready_get(&instance.config) {
+                Ok(_) => instance.clear_failed(),
+                Err(_) => instance.bump_failed(),
+            }
+        }
+    }
+}
 
 // stores the global configuration of the module
 #[derive(Debug, Clone)]
@@ -16,20 +78,108 @@ pub struct Device {
     pub enable_set_attribute_value: bool,
 }
 
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone, Default, PartialEq, Eq)]
 pub enum InstanceState {
     #[default]
     Working,
     Failed {
-        retry_count: usize,
+        retry_count: u8,
         last_retry_at: Instant,
     },
+}
+
+impl InstanceState {
+    pub fn new_failed() -> InstanceState {
+        InstanceState::Failed {
+            retry_count: 0,
+            last_retry_at: Instant::now(),
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
 pub struct InstanceData {
     pub config: Configuration,
     pub state: Arc<RwLock<InstanceState>>,
+}
+
+pub enum InstanceAttempt {
+    /// The instance is in the failed state and should not be used
+    Failed,
+    /// The instance is in the failed  state but a connection should be attempted
+    Retry,
+    /// The instance is in the working state
+    Working,
+}
+
+impl InstanceData {
+    pub fn should_try(&self) -> InstanceAttempt {
+        let this = self.state.read().unwrap();
+        match *this {
+            InstanceState::Working => InstanceAttempt::Working,
+            InstanceState::Failed {
+                retry_count,
+                last_retry_at,
+            } => {
+                if last_retry_at.elapsed() < retry_duration_from_count(retry_count) {
+                    InstanceAttempt::Failed
+                } else {
+                    InstanceAttempt::Retry
+                }
+            }
+        }
+    }
+
+    pub fn clear_failed(&self) {
+        *self.state.write().unwrap() = InstanceState::Working;
+    }
+
+    pub fn bump_failed(&self) {
+        let mut write = self.state.write().unwrap();
+        let retry_count = match *write {
+            InstanceState::Working => {
+                *write = InstanceState::new_failed();
+                0
+            }
+            InstanceState::Failed {
+                retry_count: prev_retry_count,
+                last_retry_at,
+            } => {
+                // We only bump if it's a "real" retry. This is to avoid race conditions where
+                // the same instance stops working when multiple threads are simultaneously connecting
+                // to it
+                if last_retry_at.elapsed() >= retry_duration_from_count(prev_retry_count) {
+                    let retry_count = prev_retry_count.saturating_add(1);
+                    *write = InstanceState::Failed {
+                        retry_count,
+                        last_retry_at: Instant::now(),
+                    };
+                    retry_count
+                } else {
+                    prev_retry_count
+                }
+            }
+        };
+        drop(write);
+        if THREADS_ALLOWED.load(Relaxed) {
+            RETRY_THREAD
+                .send((retry_duration_from_count(retry_count), self.clone()))
+                .ok();
+        }
+    }
+}
+
+fn retry_duration_from_count(retry_count: u8) -> Duration {
+    let secs = match retry_count {
+        0 | 1 => 1,
+        2 => 2,
+        3 => 5,
+        4 => 10,
+        5 => 60,
+        6.. => 60 * 5,
+    };
+
+    Duration::from_secs(secs)
 }
 
 #[derive(Debug, Clone)]
