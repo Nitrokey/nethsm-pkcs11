406fe5d69e1c00ac6a1ad5af6301a61902175fd1
Sosthène Guédon <sosthene@nitrokey.com>
2026-04-20 11:30:09 +0200

    Split handling in multiple functions

--- pkcs11/src/utils.rs
+++ pkcs11/src/utils.rs
@@ -1,6 +1,7 @@
 use rayon::{ThreadPool, ThreadPoolBuilder};
 use std::{
-    mem,
+    any::Any,
+    io, mem,
     sync::{
         atomic::{AtomicUsize, Ordering::Relaxed},
         Mutex, RwLock,
@@ -42,75 +43,69 @@ static DEAD_THREADS: AtomicUsize = AtomicUsize::new(0);
 static RAYON_HANDLES: Mutex<Vec<JoinHandle<()>>> = Mutex::new(Vec::new());
 static RAYON_THREADPOOL: RwLock<Option<ThreadPool>> = RwLock::new(None);
 
+fn log_thread_pool_error(err: &Box<dyn Any + Send + 'static>) {
+    if let Some(err) = err.downcast_ref::<&'static str>() {
+        log::error!("Thread pool thread panicked: {err}");
+    } else if let Some(err) = err.downcast_ref::<String>() {
+        log::error!("Thread pool thread panicked: {err}");
+    } else {
+        log::error!("Thread pool thread panicked: {err:?}");
+    }
+}
+
+fn cleanup_dead_threads(num_cpus: usize) {
+    let dead_threads = DEAD_THREADS.fetch_add(1, Relaxed);
+    // Prevent potential infinitely growing handle count
+    if dead_threads > 2 * num_cpus {
+        // Should be somewhat fast as the number of threads should be proportional to the number of CPU
+        // Only use try_lock to avoid any deadlock
+        if let Ok(mut handles) = RAYON_HANDLES.try_lock() {
+            let tmp = mem::replace(&mut *handles, Vec::with_capacity(2 * num_cpus));
+            let mut removed_threads = 0;
+            *handles = tmp
+                .into_iter()
+                .filter_map(|h| {
+                    if h.is_finished() {
+                        removed_threads += 1;
+                        h.join().inspect_err(log_thread_pool_error).ok();
+                        None
+                    } else {
+                        Some(h)
+                    }
+                })
+                .collect();
+            DEAD_THREADS.fetch_sub(removed_threads, Relaxed);
+        }
+    }
+}
+
+fn spawn_handler(num_cpus: usize, thread: rayon::ThreadBuilder) -> io::Result<()> {
+    let mut builder = std::thread::Builder::new();
+    if let Some(name) = thread.name() {
+        builder = builder.name(name.to_string());
+    }
+    if let Some(size) = thread.stack_size() {
+        builder = builder.stack_size(size);
+    }
+    let handle = builder.spawn(move || {
+        thread.run();
+        cleanup_dead_threads(num_cpus);
+    })?;
+    RAYON_HANDLES
+        .lock()
+        .expect("Rayon handles not poisoned")
+        .push(handle);
+    Ok(())
+}
+
 pub fn initialize_threadpool() {
     assert!(THREADS_ALLOWED.load(Relaxed));
 
     let num_cpus = available_parallelism().map(|m| m.get()).unwrap_or(1);
-    let threadpool = Some(
-        ThreadPoolBuilder::new()
-            .spawn_handler(move |thread| {
-                let mut builder = std::thread::Builder::new();
-                if let Some(name) = thread.name() {
-                    builder = builder.name(name.to_string());
-                }
-                if let Some(size) = thread.stack_size() {
-                    builder = builder.stack_size(size);
-                }
-                let handle = builder.spawn(move || {
-                    thread.run();
-                    let dead_threads = DEAD_THREADS.fetch_add(1, Relaxed);
-                    // Prevent potential infinitely growing handle count
-                    if dead_threads > 2 * num_cpus {
-                        // Should be somewhat fast as the number of threads should be proportional to the number of CPU
-                        // Only use try_lock to avoid any deadlock
-                        if let Ok(mut handles) = RAYON_HANDLES.try_lock() {
-                            let tmp = mem::replace(&mut *handles, Vec::with_capacity(2 * num_cpus));
-                            let mut removed_threads = 0;
-                            *handles = tmp
-                                .into_iter()
-                                .filter_map(|h| {
-                                    if h.is_finished() {
-                                        removed_threads += 1;
-                                        h.join()
-                                            .inspect_err(|err| {
-                                                if let Some(err) =
-                                                    err.downcast_ref::<&'static str>()
-                                                {
-                                                    log::error!(
-                                                        "Thread pool thread panicked: {err}"
-                                                    );
-                                                } else if let Some(err) =
-                                                    err.downcast_ref::<String>()
-                                                {
-                                                    log::error!(
-                                                        "Thread pool thread panicked: {err}"
-                                                    );
-                                                } else {
-                                                    log::error!(
-                                                        "Thread pool thread panicked: {err:?}"
-                                                    );
-                                                }
-                                            })
-                                            .ok();
-                                        None
-                                    } else {
-                                        Some(h)
-                                    }
-                                })
-                                .collect();
-                            DEAD_THREADS.fetch_sub(removed_threads, Relaxed);
-                        }
-                    }
-                })?;
-                RAYON_HANDLES
-                    .lock()
-                    .expect("Rayon handles not poisoned")
-                    .push(handle);
-                Ok(())
-            })
-            .build()
-            .expect("Failed to start rayon pool"),
-    );
+    let threadpool = ThreadPoolBuilder::new()
+        .spawn_handler(|thread| spawn_handler(num_cpus, thread))
+        .build()
+        .expect("Failed to start rayon pool");
     let mut handle = RAYON_THREADPOOL
         .try_write()
         .expect("During initialization, pool should be free");
@@ -118,7 +113,7 @@ pub fn initialize_threadpool() {
         handle.is_none(),
         "During initialization, threadpool should be empty"
     );
-    *handle = threadpool;
+    *handle = Some(threadpool);
 }
 
 pub fn run_in_threadpool<OP, R>(op: OP) -> R
@@ -139,17 +134,6 @@ pub fn close_threadpool() {
     assert!(THREADS_ALLOWED.load(Relaxed));
     *RAYON_THREADPOOL.write().unwrap() = None;
     for handle in mem::take(&mut *RAYON_HANDLES.lock().unwrap()) {
-        handle
-            .join()
-            .inspect_err(|err| {
-                if let Some(err) = err.downcast_ref::<&'static str>() {
-                    log::error!("Thread pool thread panicked: {err}");
-                } else if let Some(err) = err.downcast_ref::<String>() {
-                    log::error!("Thread pool thread panicked: {err}");
-                } else {
-                    log::error!("Thread pool thread panicked: {err:?}");
-                }
-            })
-            .ok();
+        handle.join().inspect_err(log_thread_pool_error).ok();
     }
 }
